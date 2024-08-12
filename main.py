a1b713c8a6a9c26b8202b3318281d2adf19986d6
terryproctor <terryproctor2015@outlook.com>
2024-08-12 23:00:39 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+s_data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240812.csv")
+
+red_squirrels = s_data[s_data["Primary Fur Color"] == 'Cinnamon']
+grey_squirrels = s_data[s_data["Primary Fur Color"] == 'Gray']
+black_squirrels = s_data[s_data["Primary Fur Color"] == 'Black']
+
+data_dict = {
+    "fur color" : ["red", "grey", "black"],
+    "count" : [len(red_squirrels), len(grey_squirrels), len(black_squirrels)] 
+}
+
+s_df = pd.DataFrame(data_dict).sort_values("count", ascending=False)
+
+print(s_df)
